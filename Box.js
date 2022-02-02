ac45569becae3ee8911e7622c489d08cd9fce475
Edwedja de Lima <90485665+Edwedja@users.noreply.github.com>
2022-02-02 11:14:39 -0300

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,13 @@
+class Box {
+  constructor(x, y, width, height) {
+    var options = {
+        'restitution':0.8,
+        'friction':1.0,
+        'density':0.04,
+    }
+    
+  }
+  display(){
+    
+  }
+}
